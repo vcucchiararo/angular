d265b48e4f4f06138db04f72bb3498244c34c72f
Vanesa Gómez <v.cucchiararo@gmail.com>
2020-10-22 11:40:51 +0200

    optional, default and required arguments

--- typescript/app.ts
+++ typescript/app.ts
@@ -1,16 +1,1 @@
-(function () {
-  function getAge() {
-    return 100 + 100 + 300;
-  }
-  const NAME = "Vanesa";
-  const LASTNAME = "Gomez";
-  const AGE = 30;
-
-  const person = `
-  ${NAME} 
-  ${LASTNAME} 
-  (${getAge()})`;
-  //Vanesa Gomez (Age: 30);
-
-  console.log(person);
-})();
+(function () {})();
